d9846120897f0b1bbf3171719b65bf0cc5f525f5
Kamal-Act <01388@mml.local>
2026-04-30 09:56:09 +0530

    Add homepage redirect to FD dashboard

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect   # 👈 ADD THIS
 
 from django.conf import settings
 from django.conf.urls.static import static
 
+# 👇 ADD THIS FUNCTION
+def home(request):
+    return redirect('/fd/dashboard/')
+
 urlpatterns = [
+    path('', home),   # 👈 ADD THIS LINE
     path("admin/", admin.site.urls),
     path("fd/", include("fd.urls")),
 ]
